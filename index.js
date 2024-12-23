c666af9a74f3fa92d07c7aa09406d9aa7891da0b
GiorgiGogaladze <gogaladzegio55@gmail.com>
2024-12-23 14:25:48 +0400

    Ftr: dislpaying enemies on the screen bu adding some functions

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+import EnemyController from "./EnemyController.js";
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 
-canvas.width = 500;
+canvas.width = 600;
 canvas.height  = 500;
 
 const background = new Image();
 background.src = 'images/spc.jpeg';
 
+const enemyController = new EnemyController(canvas);
+
 function game() { 
-    ctx.drawImage(background, 0, 0, canvas.width, canvas.height)
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+    enemyController.draw(ctx);
 }
 
 setInterval(game, 1000/60);

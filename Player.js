b0022550106851bc2ee25a1abea389e2f5adf350
GiorgiGogaladze <gogaladzegio55@gmail.com>
2024-12-26 15:38:29 +0400

    Ftr: adding movements to the spaceship

--- Player.js
+++ Player.js
@@ -1,4 +1,8 @@
 export default class Player {
+
+    rigthPressed = false;
+    leftPressed = false;
+
     constructor(canvas, velocity){
         this.canvas = canvas;
         this.velocity = velocity;
@@ -9,9 +13,49 @@ export default class Player {
         this.height = 48;
         this.image = new Image();
         this.image.src = 'images/player.png';
+
+        document.addEventListener('keydown', this.keydown);
+        document.addEventListener('keyup', this.keyup);
+
     }
 
     draw(ctx) {
+        this.move();
+        this.collideWithWalls();
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    }
+
+    collideWithWalls(){
+        if(this.x < 0){
+            this.x = 0;
+        }
+        if(this.x > this.canvas.width-this.width){
+            this.x = this.canvas.width - this.width;
+        }
+    }
+    move() {
+        if( this.rigthPressed){
+            this.x += this.velocity
+        }
+        else if (this.leftPressed){
+            this.x += -this.velocity;
+        }
+    }
+
+    keydown = event => {
+        if(event.code == 'ArrowRight'){
+            this.rigthPressed = true;
+        }
+        else if(event.code == 'ArrowLeft'){
+            this.leftPressed = true;
+        }
+    }
+
+    keyup = event => {
+        if(event.code == 'ArrowRight'){
+            this.rigthPressed = false;
+        } else if(event.code == 'ArrowLeft'){
+            this.leftPressed = false;
+        }
     }
 }
